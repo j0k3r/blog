78cacec9387efc9a9ecd91ef85af783edeee9557
Jeremy Benoist <jeremy.benoist@gmail.com>
2025-01-09 15:48:22 +0100

    Rebuild using 11ty

---
+++ _data/metadata.js
@@ -0,0 +1,11 @@
+export default {
+	title: "j0k3r.n3t",
+	url: "https://www.j0k3r.net/",
+	language: "fr",
+	description: "I am writing about my experiences as a naval navel-gazer.",
+	author: {
+		name: "Jérémy Benoist",
+		email: "youremailaddress@example.com",
+		url: "https://www.j0k3r.net/about"
+	}
+}
---
+++ content/blog/blog.11tydata.js
@@ -0,0 +1,7 @@
+export default {
+	tags: [
+		"posts"
+	],
+	"layout": "layouts/post.njk",
+	"permalink": "blog/{{ page.fileSlug }}/",
+};
